46556ce29c9dc00bc6251afbe53879fb75cce4f0
Phawit Pornwattanakul <me@helloyeew.dev>
2022-01-30 12:06:14 +0700

    Finish POST on a new file

--- routes/jsonapi.js
+++ routes/jsonapi.js
@@ -16,10 +16,24 @@ const dataApiRoutes = (app, fs) => {
                 logger.error("Error in readFile operation " + dataApiRoutes.name);
                 logger.error('Filename : ' + filePath);
                 logger.error('Detail : ' + err);
-                callback(null);
-            } else {
-                callback(returnJson ? JSON.parse(data) : data);
+                logger.info('Creating file ' + filePath);
+                fs.writeFile(filePath, '{}', (err) => {
+                    if (err) {
+                        logger.error("Error in writeFile operation " + dataApiRoutes.name);
+                        logger.error('Filename : ' + filePath)
+                        logger.error('Detail : ' + err)
+                    } else {
+                        logger.info('Database file created');
+                    }
+                });
+                callback(err, null);
             }
+            try {
+                callback(returnJson ? JSON.parse(data) : data);
+            } catch(err) {
+                callback({});
+            }
+
         });
     };
 
@@ -33,7 +47,7 @@ const dataApiRoutes = (app, fs) => {
             if (err) {
                 throw err;
             }
-            callback();
+            callback({});
         });
     };
 
@@ -66,31 +80,17 @@ const dataApiRoutes = (app, fs) => {
             // TODO: Make log more verbose
             const newDataId = Date.now().toString();
 
-            if (fs.existsSync(ApiFilePath)) {
-                console.log(`data filename:${req.params['filename']} updated`);
-                logger.info(ApiFilePath)
-
-                // Add a new data
-                data[newDataId] = req.body;
-
-                // TODO: Make response more meaningful
-                writeFile(JSON.stringify(data, null, 2), () => {
-                    res.status(200).send('new data added');
-                    logger.info("Data added")
-                }, ApiFilePath);
-            } else {
-                console.log(`data filename:${req.params['filename']} not found, create a new one...`);
-                // Create database file
-                fs.writeFile(ApiFilePath, '{}', (err) => {
-                    if (err) {
-                        logger.error("Error in POST request")
-                        logger.error('Filename : ' + req.params['filename'])
-                        logger.error('Detail : ' + err)
-                    } else {
-                        logger.info('Database file created');
-                    }
-                });
+            if (!fs.existsSync(ApiFilePath)) {
+                logger.warn(`data filename:${req.params['filename']} not found, create a new one...`);
             }
+            // Add a new data
+            data[newDataId] = req.body;
+
+            // TODO: Make response more meaningful
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send('new data added');
+                logger.info("Data added")
+            }, ApiFilePath);
         }, true, ApiFilePath);
     });
 
